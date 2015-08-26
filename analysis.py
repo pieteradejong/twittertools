1aa4c2ae722edf779d14e413f4b72c68558c91c3
Pieter de Jong <pieterdejong@Pieters-MacBook-Air.local>
2015-08-25 23:22:55 -0700

    fetches and prints user followers and followees user names

--- analysis.py
+++ analysis.py
@@ -8,6 +8,10 @@ api = twitter.Api(
  access_token_secret=env.config['access_token_secret']
  )
 
+print "Verifying Twitter API Credentials.."
+print api.VerifyCredentials()
+print "(DONE) Twitter API Credentials Verified"
+
 
 search = api.GetSearch(term='technology', lang='en', result_type='recent', count=100, max_id='')
 for t in search:
@@ -15,3 +19,18 @@ for t in search:
  #Add the .encode to force encoding
  print t.text.encode('utf-8')
  print ''
+
+
+print "Getting all followers' user names\n"
+followers = api.GetFollowers()
+for fol in followers:
+  print fol.screen_name
+print "(DONE) Getting all followers' user names\n\n\n"
+
+print "Getting all followees' user names\n"
+friends = api.GetFriends()
+for friend in friends:
+  print friend.screen_name
+print "(DONE) Getting all followees' user names\n\n\n"
+
+
